2dadc35bd8b3568dd012501c51efb33d56e5974d
sibelkarasioglu <sibelkarasioglu@hotmail.com>
2020-11-01 10:39:49 -0500

    initial commit

---
+++ src/main/java/FirstDay.java
@@ -0,0 +1,6 @@
+public class FirstDay {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
---
+++ src/test/java/FirstDayTest.java
@@ -0,0 +1,5 @@
+public class FirstDayTest {
+    public static void main(String[] args) {
+
+    }
+}
